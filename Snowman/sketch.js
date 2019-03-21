5cb96e36943508ff76f82b2ce124f19cfc0fef32
T Walker <48356536+mhsTWalker@users.noreply.github.com>
2019-03-21 09:46:14 -0400

    Add files via upload

---
+++ Snowman/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  // put setup code here
+ //Create a canvasnew
+ createCanvas(800,600);
+}
+function draw() {
+  //create variables for the width, height,x, y values
+
+
+  // put drawing code here
+  rect(1.5,480,789,100)//floor
+
+  ellipse(400, 420, 200, 200);//bottom
+  ellipse(400, 320, 140, 140);//midle
+  ellipse(400, 220, 100, 100);//top
+  line(450, 325, 540, 250)//left arm
+  line(330, 325, 250, 250)//right arm
+}
