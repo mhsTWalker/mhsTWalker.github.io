59a973b7b9ecd6d8fa64b3dd9864f595e86477a5
T Walker <48356536+mhsTWalker@users.noreply.github.com>
2019-05-10 09:20:27 -0400

    Add files via upload

---
+++ LoopingStructures/sketch.js
@@ -0,0 +1,28 @@
+
+function setup() {
+  // put setup code here
+  createCanvas(1280,877);
+}
+
+function draw() {
+  // put drawing code here
+  background(0);
+  strokeWeight(4);
+  stroke(255);
+
+  var x = 0;
+
+  while(x < width){
+    fill(0,200,255);
+    ellipse(x,100,25,25);
+    x = x + 50
+  }
+
+
+}
+
+  for(var x = 0 x < width, x = x +50){
+      fill(255,0,200);
+      ellipse(x,100,25,25);
+
+  }
